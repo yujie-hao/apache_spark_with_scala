c01740ef07420972dd041ffbb22be2e67e6caa1d
yujie-hao <haoyj20@gmail.com>
2021-05-20 21:16:52 -0400

    added ml-100k data folder HelloWorld works added README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+Apache Spark with Scala tutorial
+
+[link to tutorial](https://www.udemy.com/course/apache-spark-with-scala-hands-on-with-big-data)
---
+++ src/main/scala/com/sundogsoftware/spark/HelloWorld.scala
@@ -0,0 +1,22 @@
+package com.sundogsoftware.spark
+
+import org.apache.spark._
+import org.apache.log4j._
+
+//JDK 14 works.
+//JDK 16 does not work.
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val sc = new SparkContext("local[*]", "HelloWorld")
+
+    val lines = sc.textFile("data/ml-100k/u.data")
+    val numLines = lines.count()
+
+    println("Hello world! The u.data file has " + numLines + " lines.")
+
+    sc.stop()
+  }
+}
